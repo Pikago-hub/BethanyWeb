1784f070840795664c55b6c07ad4bb10c6003670
Pikago <wuxiaozhu9821@hotmail.com>
2022-12-22 02:27:09 -0600

    heroku config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-
-module.exports = ({
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-    disableHostCheck: true
+    allowedHosts: "all",
   }
 })
