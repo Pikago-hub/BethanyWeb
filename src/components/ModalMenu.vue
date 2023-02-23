006187f50b5c824a38aff2fdc07b115a775784a0
AlbertoIreneoGaucin <albertogaucin.ag@gmail.com>
2023-02-23 11:47:23 -0600

    modal

---
+++ src/components/ModalMenu.vue
@@ -0,0 +1,126 @@
+<template>
+    
+    <div class="modal-overlay" @click="$emit('close-modal')">
+      <div class="card-modal" >
+        <div class="card">
+            <div>
+                <h6>Title</h6>
+                <p>Description</p>
+            </div>
+            <div class="image">
+                <img class="check" src="../assets/images/bethany.png" alt="" />
+            </div>
+        </div>
+        <div class="card">
+            <div>
+                <h6>Title</h6>
+                <p>Description</p>
+            </div>
+            <div class="image">
+                <img class="check" src="../assets/images/bethany.png" alt="" />
+            </div>
+        </div>
+        <div class="card">
+            <div>
+                <h6>Title</h6>
+                <p>Description</p>
+            </div>
+            <div class="image">
+                <img class="check" src="../assets/images/bethany.png" alt="" />
+            </div>
+        </div>
+        <div class="card">
+            <div>
+                <h6>Title</h6>
+                <p>Description</p>
+            </div>
+            <div class="image">
+                <img class="check" src="../assets/images/bethany.png" alt="" />
+            </div>
+        </div>
+        <ModalMenuVue dishes="dishes" v-show="showModal" @close-modal="showModal = false"/>
+        <button  @click="$emit('close-modal')">Back</button>
+      </div>
+     
+      
+    </div>
+  </template>
+  
+  <script>
+    export default {
+        props: ['dishes'],
+        created() {
+    // props are exposed on `this`
+        console.log(this.dishes)
+  }
+  }
+  </script>
+  <style scoped>
+
+
+  .modal-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    background-color: #000000da;
+    
+  }
+  
+  .card-modal {
+    text-align: center;
+    background-color: white;
+    height: 500px;
+    width: 500px;
+    margin-top: 20%;
+    padding: 60px 0;
+    border-radius: 20px;
+    overflow-y:auto;
+  }
+  .card{
+    display: flex;
+    flex-direction: row;
+   
+  }
+  .image{
+    margin-left: 0.5 rem;
+  }
+  .close {
+    margin: 10% 0 0 16px;
+    cursor: pointer;
+  }
+  
+  .close-img {
+    width: 25px;
+  }
+  .check {
+  width: 150px;
+}
+
+h6 {
+  font-weight: 500;
+  font-size: 28px;
+  margin: 20px 0;
+}
+
+p {
+  font-size: 16px;
+  margin: 20px 0;
+}
+
+button {
+  background-color: #ac003e;
+  width: 150px;
+  height: 40px;
+  color: white;
+  font-size: 14px;
+  border-radius: 16px;
+  margin-top: 50px;
+  
+  cursor: pointer;
+}
+
+</style>
--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -1,39 +1,76 @@
 <template>
   <div class="categories-grid">
-    <div v-for="(item, index) in categories" :key="index" class="category">
-      <div class="category-content">
+    <div v-for="(item, index) in categories" :key="index" class="category" @click="openModal(item)">
+      <div class="category-content" >
         <h2>{{ item.name }}</h2>
         <img
           v-if="item.image"
           :src="imageUrlFor(item.image).ignoreImageParams().width(240)"
           :alt="item.image.alt"
         />
+        
       </div>
+      
+    </div>
+    
+  </div>
+  
+  <div v-show="showModal">
+    
+    <div class="modal-overlay" @click="closeModal">
+      <div class="card-modal" >
+        <div v-for="(item, index) in dishes" :key="index" class="card">
+            <div>
+                <h6>{{item.name}}</h6>
+                <p>{{ item.short_description }}</p>
+            </div>
+            <div class="image">
+                <img class="check"  v-if="item.image"
+                  :src="imageUrlFor(item.image).ignoreImageParams().width(240)"
+                  :alt="item.image.alt" />
+            </div>
+        </div>
+        
+       
+        <button  @click="closeModal">Back</button>
+      </div>
+     
+      
     </div>
   </div>
+  
 </template>
 
 <script>
 import sanity from '../sanity'
 import imageUrlBuilder from '@sanity/image-url'
 
+
 const imageBuilder = imageUrlBuilder(sanity)
+//gets the "restaurants" which just are the 10 categories, then also gets the dishes belonging to each one
 
 const query = `
-      *[_type == "category"]
+*[_type == 'restaurant'] {
+        ...,
+       dishes[]->{
+        ...,
+      }
+         }
       `
-
 export default {
   name: 'Categories',
   data() {
     return {
       loading: true,
       categories: [],
+      dishes:[],
+      showModal: false,
     }
   },
   created() {
     this.fetchData()
   },
+  
   watch: {
     $route: 'fetchData',
   },
@@ -48,12 +85,26 @@ export default {
         (categories) => {
           this.loading = false
           this.categories = categories
+          //don't need this
+          this.dishes = categories[0].dishes
         },
         (error) => {
           this.error = error
         },
       )
     },
+    
+    openModal(item){
+      
+      this.dishes = item.dishes
+      
+      this.showModal = true
+      
+      
+    },
+    closeModal(){
+      this.showModal = false
+    }
   },
 }
 </script>
@@ -70,6 +121,7 @@ export default {
   border-radius: 5px;
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
   overflow: hidden;
+  cursor: pointer;
 }
 
 .category-content {
@@ -81,4 +133,71 @@ export default {
   height: auto;
   margin-bottom: 1rem;
 }
+  .modal-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    background-color: #000000da;
+    
+  }
+  
+  .card-modal {
+    text-align: center;
+    background-color: white;
+    height: 500px;
+    width: 500px;
+    margin-top: 20%;
+    padding: 60px 0;
+    border-radius: 20px;
+    overflow-y:auto;
+  }
+  .card{
+    display: flex;
+    flex-direction: row;
+   
+  }
+  .image{
+    margin-left: 0.5 rem;
+  }
+  .close {
+    margin: 10% 0 0 16px;
+    cursor: pointer;
+  }
+  
+  .close-img {
+    width: 25px;
+  }
+  .check {
+  width: 150px;
+}
+
+h6 {
+  font-weight: 500;
+  font-size: 28px;
+  margin: 20px 0;
+}
+
+p {
+  font-size: 16px;
+  margin: 20px 0;
+}
+
+button {
+  background-color: #ac003e;
+  width: 150px;
+  height: 40px;
+  color: white;
+  font-size: 14px;
+  border-radius: 16px;
+  margin-top: 50px;
+  
+  cursor: pointer;
+}
+
+
+
 </style>
