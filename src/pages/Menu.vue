30b486b8258eddda62d0bf99d264271f81723f02
edwardrand <52064142+tanmay-kali@users.noreply.github.com>
2023-02-24 02:07:52 -0600

    UI FIXES

--- src/pages/Menu.vue
+++ src/pages/Menu.vue
@@ -1,50 +1,54 @@
 <template>
   <div class="categories-grid">
-    <div v-for="(item, index) in categories" :key="index" class="category" @click="openModal(item)">
-      <div class="category-content" >
-        <h2>{{ item.name }}</h2>
-        <img
-          v-if="item.image"
-          :src="imageUrlFor(item.image).ignoreImageParams().width(240)"
-          :alt="item.image.alt"
-        />
-        
-      </div>
-      
-    </div>
-    
-  </div>
-  
-  <div v-show="showModal">
-    
-    <div class="modal-overlay" @click="closeModal">
-      <div class="card-modal" >
-        <div v-for="(item, index) in dishes" :key="index" class="card">
-            <div>
-                <h6>{{item.name}}</h6>
-                <p>{{ item.short_description }}</p>
-            </div>
-            <div class="image">
-                <img class="check"  v-if="item.image"
-                  :src="imageUrlFor(item.image).ignoreImageParams().width(240)"
-                  :alt="item.image.alt" />
-            </div>
+    <div
+      v-for="(item, index) in categories"
+      :key="index"
+      class="category"
+      @click="openModal(item)"
+    >
+      <div class="card">
+        <div class="image">
+          <img
+            v-if="item.image"
+            :src="imageUrlFor(item.image)"
+            :alt="item.image.alt"
+            width="240"
+          />
         </div>
-        
-       
-        <button  @click="closeModal">Back</button>
+        <div class="category-content">
+          <h2>{{ item.name }}</h2>
+        </div>
       </div>
-     
-      
     </div>
   </div>
-  
+
+  <div v-show="showModal">
+    <div class="modal-overlay" @click="closeModal">
+      <div class="card-modal">
+        <div v-for="(item, index) in dishes" :key="index" class="card">
+          <div>
+            <h6>{{ item.name }}</h6>
+            <p>{{ item.short_description }}</p>
+          </div>
+          <div class="image">
+            <img
+              class="check card-img"
+              v-if="item.image"
+              :src="imageUrlFor(item.image).ignoreImageParams().width(240)"
+              :alt="item.image.alt"
+            />
+          </div>
+        </div>
+
+        <button @click="closeModal">Back</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import sanity from '../sanity'
 import imageUrlBuilder from '@sanity/image-url'
-
 
 const imageBuilder = imageUrlBuilder(sanity)
 //gets the "restaurants" which just are the 10 categories, then also gets the dishes belonging to each one
@@ -63,14 +67,14 @@ export default {
     return {
       loading: true,
       categories: [],
-      dishes:[],
+      dishes: [],
       showModal: false,
     }
   },
   created() {
     this.fetchData()
   },
-  
+
   watch: {
     $route: 'fetchData',
   },
@@ -93,18 +97,15 @@ export default {
         },
       )
     },
-    
-    openModal(item){
-      
+
+    openModal(item) {
       this.dishes = item.dishes
-      
+
       this.showModal = true
-      
-      
     },
-    closeModal(){
+    closeModal() {
       this.showModal = false
-    }
+    },
   },
 }
 </script>
@@ -112,8 +113,9 @@ export default {
 <style scoped>
 .categories-grid {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(300px, calc(100% / 3 - 2rem)));
   gap: 2rem;
+  padding: 3rem;
 }
 
 .category {
@@ -133,45 +135,49 @@ export default {
   height: auto;
   margin-bottom: 1rem;
 }
-  .modal-overlay {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    background-color: #000000da;
-    
-  }
-  
-  .card-modal {
-    text-align: center;
-    background-color: white;
-    height: 500px;
-    width: 500px;
-    margin-top: 20%;
-    padding: 60px 0;
-    border-radius: 20px;
-    overflow-y:auto;
-  }
-  .card{
-    display: flex;
-    flex-direction: row;
-   
-  }
-  .image{
-    margin-left: 0.5 rem;
-  }
-  .close {
-    margin: 10% 0 0 16px;
-    cursor: pointer;
-  }
-  
-  .close-img {
-    width: 25px;
-  }
-  .check {
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #000000da;
+}
+
+.card-modal {
+  text-align: center;
+  background-color: white;
+  height: 500px;
+  width: 500px;
+  margin-top: 20%;
+  padding: 60px 0;
+  border-radius: 20px;
+  overflow-y: auto;
+}
+
+.card {
+  display: flex;
+  flex-direction: row;
+}
+
+.image {
+  margin-left: 0.5 rem;
+  padding: 2rem;
+}
+
+.close {
+  margin: 10% 0 0 16px;
+  cursor: pointer;
+}
+
+.close-img {
+  width: 25px;
+}
+
+.check {
   width: 150px;
 }
 
@@ -179,25 +185,26 @@ h6 {
   font-weight: 500;
   font-size: 28px;
   margin: 20px 0;
+  color: #1a1a1a;
 }
 
 p {
   font-size: 16px;
   margin: 20px 0;
+  color: #6b6b6b;
 }
 
 button {
-  background-color: #ac003e;
+  background-color: #ff2d55;
   width: 150px;
   height: 40px;
   color: white;
   font-size: 14px;
   border-radius: 16px;
   margin-top: 50px;
-  
   cursor: pointer;
+  border: none;
+  text-transform: uppercase;
+  font-weight: 600;
 }
-
-
-
 </style>
